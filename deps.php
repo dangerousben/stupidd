0abdaf358d161911d3180aa308d41915a121feb4
Ben Spencer <dangerous.ben@gmail.com>
2015-12-06 13:25:39 +0000

    Bit of a tidy-up.

--- deps.php
+++ deps.php
@@ -3,7 +3,7 @@
 class deps {
     private $deps = [];
 
-    function instantiate($class) {
+    public function instantiate($class) {
         if (!is_subclass_of($class, "unit")) {
             throw new Exception(
                 "Sorry, I can only instantiate units for entirely arbitrary reasons that you wouldn't understand"
@@ -13,33 +13,38 @@ class deps {
         $rfClass = new ReflectionClass($class);
         $rfConstructor = $rfClass->getConstructor();
 
-        $args = [];
-
-        if ($rfConstructor) {
-
-            foreach($rfConstructor->getParameters() as $param) {
-                $depClass = $param->getClass();
-                if (!$depClass || !$depClass->isSubclassOf("unit")) {
-                    throw new Exception(
-                        "Sorry, again for entirely arbitrary reasons all your dependencies must be units - everything is a unit!"
-                    );
-                }
-
-                $name = $depClass->getName();
-
-                if (isset($this->deps[$name])) {
-                    $args[] = $this->deps[$name];
-                } else {
-                    $args[] = $this->instantiate($name);
-                }
-            }
-        }
-
-        foreach($args as $arg) $arg->start();
+        $args = $rfConstructor ? $this->getDeps($rfConstructor) : [];
 
         $dep = $rfClass->newInstanceArgs($args);
         $this->deps[$class] = $dep;
         return $dep;
     }
-}
 
+    private function getDeps(ReflectionMethod $rfConstructor) {
+        $depNames = [];
+
+        foreach($rfConstructor->getParameters() as $param) {
+            $depClass = $param->getClass();
+            if (!$depClass || !$depClass->isSubclassOf("unit")) {
+                throw new Exception(
+                    "Sorry, again for entirely arbitrary reasons all your dependencies must be units - everything is a unit!"
+                );
+            }
+
+            $depNames[] = $depClass->getName();
+        }
+
+        // to be parallelised if php ever becomes a proper language
+        return array_map([$this, "getDep"], $depNames);
+    }
+
+    private function getDep($name) {
+        if (isset($this->deps[$name])) {
+            $dep = $this->deps[$name];
+        } else {
+            $dep = $this->instantiate($name);
+        }
+        $dep->start();
+        return $dep;
+    }
+}
