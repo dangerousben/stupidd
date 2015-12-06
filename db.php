6521adbe618af99c016763815b775d2831c06512
Ben Spencer <dangerous.ben@gmail.com>
2015-12-06 12:53:56 +0000

    Home dir should be a unit too!

--- db.php
+++ db.php
@@ -1,14 +1,17 @@
 <?php
 
 class db extends unit {
+    private $dsn;
     private $pdo;
 
-    public function __construct() {}
-
-    public function doStart() {
+    public function __construct(home_dir $homeDir) {
         // Not entirely convinced that sqlite has the grunt needed to power an init
         // system but we haven't figured out a way to bring up postgres before init yet
-        $this->pdo = new PDO("sqlite:" . STUPID_DIR . "/stupid.sqlite3");
+        $this->dsn = "sqlite:$homeDir/stupid.sqlite3";
+    }
+
+    public function doStart() {
+        $this->pdo = new PDO($this->dsn);
 
         // Admittedly the default value of SILENT makes for a quieter life but let's
         // roll with this for now
